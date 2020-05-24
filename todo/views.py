16b547f55446154e8e449766c81787d79fa1d571
AnKiWoong <ankiwoong@gmail.com>
2020-05-24 16:39:35 +0900

    뷰 만들기

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .forms import TodoForm
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Todo
+
+
+def todo_fetch(request):
+    todos = Todo.objects.all()
+    todo_list = []
+    for index, todo in enumerate(todos, start=1):
+        todo_list.append({'id': index, 'title': todo.title,
+                          'completed': todo.completed})
+
+    return JsonResponse(todo_list, safe=False)
+
+
+@csrf_exempt
+def todo_save(request):
+    if request.body:
+        data = json.loads(request.body)
+        if 'todos' in data:
+            todos = data['todos']
+            Todo.objects.all().delete()
+            for todo in todos:
+                print('todo', todo)
+                form = TodoForm(todo)
+                if form.is_valid():
+                    form.save()
+
+    return JsonResponse({})
